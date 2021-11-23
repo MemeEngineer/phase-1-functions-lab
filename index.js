03b3698f29b73b4d2970b0a4b0b70ba0c3f98362
Johnny Wu <johnnyw131@gmail.com>
2021-11-23 09:31:29 -0500

    commit tree a89e3e3dc139776de6a191d20dfc26f505fef867

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(X) {
+    return Math.abs(X - 42);
+  }
+  distanceFromHqInBlocks();
+
+  function distanceFromHqInFeet(X) {
+    distanceFromHqInBlocks(X);
+    return Math.abs(X - 42) *264;
+  }
+
+  function distanceTravelledInFeet(X,Y) {
+    distanceFromHqInBlocks(X);
+    return Math.abs(X - Y) *264;
+  }
+
+
+  function calculatesFarePrice(X,Y) {
+    const dist = distanceTravelledInFeet(X,Y);
+   if (dist <= 400){
+       return dist * 0;
+   }else if (dist > 400 && dist < 2000){
+        return (dist - 400) *.02;
+   } else if (dist > 2500) {
+       return 'cannot travel that far';
+   }else if (dist >2000) {
+     return 25;
+   }
+}
